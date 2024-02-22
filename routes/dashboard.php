5bcdeaca3261168b142d2c6df1a3c8c0cc9a3735
MAHESH2056 <maheschy2323@gmaill.com>
2024-02-22 08:02:16 +0545

    Create deashboard and using css in login page

---
+++ routes/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online voting system - Dashboard</title>
+</head>
+<body>
+    <button>Back</button>
+    <button>Logout</button>
+    <h1>Online Voting System</h1>
+    <hr>
+    <!--Div Start-->
+    <div id="Profile"></div>
+    <div id="Group"></div>
+    <!--Div End-->
+</body>
+</html>
